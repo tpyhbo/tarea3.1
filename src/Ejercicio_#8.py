19d9d45744650c93564b868c34a9201e143f77a0
Jesus Pena <tpyhbo@gmail.com>
2025-03-17 13:14:10 -0600

    Operaciones matemáticas (8 - 9)

---
+++ src/Ejercicio_#8.py
@@ -0,0 +1,5 @@
+#8 Escribe una suma, resta, multiplicación y división cuyo resultado sea 8.
+print(2 + 6)
+print(16 - 8)
+print(2 * 2 * 2)
+print(64 / 8)
---
+++ src/Ejercicio_#9.py
@@ -0,0 +1,3 @@
+#9 Almacena tu número favorito en una variable y crea un mensaje que revele tu número favorito.
+num_fav = 42
+print(f"Mi número favorito es el {num_fav}.")
